37b0ad108dfef8511fd6b7d095974cdebbf1fdb5
magatec <magatec_edu@mail.ru>
2022-10-12 00:43:49 +0200

    Revert "HW_1"
    This reverts commit e54d7eed6c688b67b1b189c10c6f0f9bf7da81e7.

--- src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
@@ -1,6 +1,5 @@
 package ru.javawebinar.topjava.util;
 
-import ru.javawebinar.topjava.MealTestData;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.MealTo;
 
@@ -27,10 +26,6 @@ public class MealsUtil {
 
         List<MealTo> mealsTo = filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
         mealsTo.forEach(System.out::println);
-
-        System.out.println("*********************************");
-        List<MealTo> mealsToLimit = filteredByLimitCalories(meals, MealTestData.LIMIT_CALORIES);
-        mealsToLimit.forEach(System.out::println);
     }
 
     public static List<MealTo> filteredByStreams(List<Meal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
@@ -48,16 +43,5 @@ public class MealsUtil {
 
     private static MealTo createTo(Meal meal, boolean excess) {
         return new MealTo(meal.getDateTime(), meal.getDescription(), meal.getCalories(), excess);
-    }
-
-    public static List<MealTo> filteredByLimitCalories(List<Meal> meals, int caloriesPerDay) {
-        Map<LocalDate, Integer> caloriesSumByDate = meals.stream()
-                .collect(
-                        Collectors.groupingBy(Meal::getDate, Collectors.summingInt(Meal::getCalories))
-                );
-
-        return meals.stream()
-                .map(meal -> createTo(meal, caloriesSumByDate.get(meal.getDate()) > caloriesPerDay))
-                .collect(Collectors.toList());
     }
 }
--- src/main/webapp/users.jsp
+++ src/main/webapp/users.jsp
@@ -7,6 +7,5 @@
 <h3><a href="index.html">Home</a></h3>
 <hr>
 <h2>Users</h2>
-<h3><a href="meals">Meals list</a></h3>
 </body>
 </html>
