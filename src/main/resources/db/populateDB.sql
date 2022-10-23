4de555dba7070ebcfa6e285cbd13d4ed60f81c99
magatec <magatec_edu@mail.ru>
2022-10-23 02:09:41 +0200

    Apply patch 3_13

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,7 +1,5 @@
-DELETE
-FROM user_roles;
-DELETE
-FROM users;
+DELETE FROM user_roles;
+DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
